915c1cab601d1279794e185c99c2b1ccc513072f
Illia Shestakov <illia@ishestakov.com>
2023-09-07 14:30:37 +0300

    Bump version

--- abstract_scorm_xblock/abstract_scorm_xblock/__init__.py
+++ abstract_scorm_xblock/abstract_scorm_xblock/__init__.py
@@ -5,4 +5,4 @@
 __author__ = "Alexander Marenich, Andrey Kryachko, Andrey Lykhoman, COTOHA, David Baumgold, Volodymyr Bergman, Maxim Starodubcev, Jorge Mora, Silvio Tomatis, kirkerafael, Oksana Slusarenro, Chiruzzi Marco"
 __maintainer__ = "Silvio Tomatis, Chiruzzi Marco"
 __email__ = "silviot@gmail.com, chiruzzi.marco@gmail.com"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
